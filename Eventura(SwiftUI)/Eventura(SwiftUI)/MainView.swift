5f496be7c45f58637c99b89394a397f5e38f978d
AnshulDalua <adalua@umich.edu>
2023-03-19 17:59:46 -0400

    QR update

--- Eventura(SwiftUI)/Eventura(SwiftUI)/MainView.swift
+++ Eventura(SwiftUI)/Eventura(SwiftUI)/MainView.swift
@@ -1,36 +1,55 @@
-//
-//  MainView.swift
-//  Eventura(SwiftUI)
-//
-//  Created by Karthik Ganesh on 19/3/23.
-//
-
-import Foundation
 import SwiftUI
 
 struct MainView: View {
+    @State private var showCameraView = false
+    @State private var qrCodes: [(qrCode: String, metaData: String)] = []
+    
     var body: some View {
-        VStack {
-            Text("Welcome to the app!")
-                .font(.largeTitle)
-                .padding()
-            
-            Button(action: {
-                // Open the camera to scan a QR code
-                // You can implement this functionality using AVFoundation or a third-party library like CodeScanner
-            }) {
-                Text("Scan QR Code")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(8)
+        NavigationView {
+            VStack {
+                Button(action: {
+                    showCameraView = true
+                }) {
+                    Text("Scan QR Code")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                .padding([.leading, .trailing], 40)
+                .padding(.top)
+                
+                NavigationLink(destination: AllQRCodesView()) {
+                    Text("View All QR Codes")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.green)
+                        .cornerRadius(8)
+                }
+                .padding([.leading, .trailing], 40)
+                .padding(.top)
+                
+                Spacer()
             }
-            .padding([.leading, .trailing], 40)
-            .padding(.top)
-            
-            Spacer()
+            .navigationBarTitle("Main")
+            .sheet(isPresented: $showCameraView) {
+                CameraView()
+            }
+            .onReceive(NotificationCenter.default.publisher(for: Notification.Name("AddQRCodeToEventList"))) { notification in
+                if let qrCodeData = notification.object as? (qrCode: String, metaData: String) {
+                    qrCodes.append(qrCodeData)
+                }
+            }
         }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
     }
 }
--- Eventura(SwiftUI)/Eventura(SwiftUI)/QRCodeDetailView.swift
+++ Eventura(SwiftUI)/Eventura(SwiftUI)/QRCodeDetailView.swift
@@ -1,12 +1,11 @@
 import SwiftUI
-import AVFoundation
 
 struct QRCodeDetailView: View {
     @Environment(\.presentationMode) var presentationMode
-
+    
     let qrCode: String
     let metaData: String
-
+    
     var body: some View {
         VStack {
             Text("QR Code Details")
@@ -20,15 +19,30 @@ struct QRCodeDetailView: View {
                 .font(.headline)
             Text(metaData)
             Spacer()
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("Close")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(8)
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Close")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                Spacer()
+                Button(action: {
+                    let qrCodeData = (qrCode: qrCode, metaData: metaData)
+                    NotificationCenter.default.post(name: Notification.Name("AddQRCodeToEventList"), object: qrCodeData)
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Add to Events List")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(8)
+                }
             }
         }
         .padding()
